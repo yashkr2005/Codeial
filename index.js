eb1ce94078ae0fe2ce7b922ba50e3be2e101590c
yash kumar <yashkr2005@gmail.com>
2022-12-13 01:07:18 +0530

    Adding of user conrollers and route setting up view engine

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const port=8000;
 //use express router
 app.use('/',require('./routes/index'));
 
+//setup of view engine
+app.set('view engine','ejs');
+app.set('views','./views');
 
 app.listen(port,function(err){
     if(err)
--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,9 @@ const router=express.Router();
 const homeController=require('../controllers/home_controllers');
 
 router.get('/',homeController.home);
+router.use('/users',require('./users'));
+
+// for any other route 
+// router.use('/routename',require('./routefilename'));
+
 module.exports=router;
