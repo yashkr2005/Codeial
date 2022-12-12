397ad46273f10aa92dbf29cc123a5b4ef5cb019b
yash kumar <yashkr2005@gmail.com>
2022-12-13 00:43:43 +0530

    Adding controllers and routes for home

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express=require('express');
+const router = require('./routes/index');
 const app=express();
 const port=8000;
+
+//use express router
+app.use('/',require('./routes/index'));
+
 
 app.listen(port,function(err){
     if(err)
